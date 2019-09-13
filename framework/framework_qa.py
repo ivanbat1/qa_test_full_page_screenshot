024b4ca404527e89cbb9d313a14a0c88542d0800
AvantisPro <proyava2296@gmail.com>
2019-09-13 18:03:14 +0300

    'make'

---
+++ framework/framework_qa.py
@@ -0,0 +1,63 @@
+import os
+from PIL import Image
+from io import BytesIO
+
+verbose = 1
+
+def full_screenshot(driver, save_path, file_name):
+    # initiate value
+    save_path = save_path + '.png' if save_path[-4::] != '.png' else save_path
+    img_li = []  # to store image fragment
+    offset = 0  # where to start
+    # js to get height
+    height = driver.execute_script('return Math.max('
+                                   'document.documentElement.clientHeight, window.innerHeight);')
+    # js to get the maximum scroll height
+    max_window_height = driver.execute_script('return Math.max('
+                                              'document.body.scrollHeight, '
+                                              'document.body.offsetHeight, '
+                                              'document.documentElement.clientHeight, '
+                                              'document.documentElement.scrollHeight, '
+                                              'document.documentElement.offsetHeight);')
+
+    # looping from top to bottom, append to img list
+    while offset < max_window_height:
+        # Scroll to height
+        driver.execute_script(f'window.scrollTo(0, {offset});')
+        img = Image.open(BytesIO((driver.get_screenshot_as_png())))
+        img_li.append(img)
+        offset += height
+    # Stitch image into one
+    # Set up the full screen frame
+    box = (0, height - height * (max_window_height / height - max_window_height // height), img_li[-1].size[0],
+           img_li[-1].size[1])
+    img_li[-1] = img_li[-1].crop(box)
+    img_frame_height = sum([img_frag.size[1] for img_frag in img_li])
+    img_frame = Image.new('RGB', (img_li[0].size[0], img_frame_height))
+    offset = 0
+    for img_frag in img_li:
+        img_frame.paste(img_frag, (0, offset))
+        offset += img_frag.size[1]
+    img_frame.save('{}'.format(file_name) + '/' + save_path)
+
+
+def get_all_url_page(driver):
+    href_list = list()
+    elems = driver.find_elements_by_xpath("//a[@href]")
+    for elem in elems:
+        if elem.get_attribute("href").startswith('http://'):
+            href_list.append(elem.get_attribute("href"))
+    return list(set(href_list))
+
+
+def go_to_page_make_screenshot(driver, url, ranger, file_name, js_file):
+    driver.get(url)
+    driver.execute_script(open(js_file).read())
+    full_screenshot(driver, 'new' + str(ranger) + '.png', file_name)
+
+
+def create_dir(file_name):
+    try:
+        os.mkdir('{}'.format(file_name))
+    except FileExistsError:
+        print('file exist')
--- test.py
+++ test.py
@@ -1,66 +1,14 @@
-from Screenshot import Screenshot_Clipping
 from selenium import webdriver
-import time
-from PIL import Image
-from io import BytesIO
+from datetime import datetime
+from framework.framework_qa import get_all_url_page, create_dir, go_to_page_make_screenshot
 
-
-def full_screenshot(driver, save_path):
-    # initiate value
-    save_path = save_path + '.png' if save_path[-4::] != '.png' else save_path
-    img_li = []  # to store image fragment
-    offset = 0  # where to start
-
-    # js to get height
-    height = driver.execute_script('return Math.max('
-                                   'document.documentElement.clientHeight, window.innerHeight);')
-    # js to get the maximum scroll height
-    # Ref--> https://stackoverflow.com/questions/17688595/finding-the-maximum-scroll-position-of-a-page
-    max_window_height = driver.execute_script('return Math.max('
-                                              'document.body.scrollHeight, '
-                                              'document.body.offsetHeight, '
-                                              'document.documentElement.clientHeight, '
-                                              'document.documentElement.scrollHeight, '
-                                              'document.documentElement.offsetHeight);')
-
-    # looping from top to bottom, append to img list
-    # Ref--> https://gist.github.com/fabtho/13e4a2e7cfbfde671b8fa81bbe9359fb
-    while offset < max_window_height:
-        # Scroll to height
-        driver.execute_script(f'window.scrollTo(0, {offset});')
-        img = Image.open(BytesIO((driver.get_screenshot_as_png())))
-        img_li.append(img)
-        offset += height
-
-    # Stitch image into one
-    # Set up the full screen frame
-    box = (0, height - height * (max_window_height / height - max_window_height // height), img_li[-1].size[0], img_li[-1].size[1])
-    img_li[-1] = img_li[-1].crop(box)
-    img_frame_height = sum([img_frag.size[1] for img_frag in img_li])
-    img_frame = Image.new('RGB', (img_li[0].size[0], img_frame_height))
-    offset = 0
-    for img_frag in img_li:
-        img_frame.paste(img_frag, (0, offset))
-        offset += img_frag.size[1]
-    img_frame.save(save_path)
-
-
-def go_to_page(url, ranger):
-    driver.get(url)
-    driver.execute_script(open("./F.js").read())
-    time.sleep(1)
-    full_screenshot(driver, 'new' + str(ranger) + '.png')
-
-
-verbose = 1
-driver = webdriver.Chrome('C:/Users/ITUA/Desktop/chromedriver/chromedriver.exe')
-driver.get("http://medlife.saprit.co.uk/about-us/")
-elems = driver.find_elements_by_xpath("//a[@href]")
-href_list = list(set())
-for elem in elems:
-    if elem.get_attribute("href").startswith('http://'):
-        href_list.append(elem.get_attribute("href"))
-i = 0
-while i != len(href_list):
-    go_to_page(href_list[i], i)
-    i += 1
+if __name__ == "__main__":
+    driver = webdriver.Chrome('C:/Users/ITUA/Desktop/chromedriver/chromedriver.exe')
+    driver.get("http://medlife.saprit.co.uk/about-us/")
+    file_name = datetime.today().date()
+    create_dir(file_name)
+    href_list = get_all_url_page(driver)
+    i = 0
+    while i != len(href_list):
+        go_to_page_make_screenshot(driver, href_list[i], i, file_name, "framework/F.js")
+        i += 1
